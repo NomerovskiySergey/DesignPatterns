0f8a1ae34863d11bcfa392f8ae3bb05689dcd09d
NomerovskiySergey <nomerushka@gmail.com>
2019-01-09 22:51:09 +0200

    FactoryMethod in process...

---
+++ DesignPatterns.Source/CreationalPatterns/FactoryMethod/Client.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using DesignPatterns.Source.BehaviourPatterns;
+
+namespace DesignPatterns.Source.CreationalPatterns.FactoryMethod
+{
+    class Client : IClient
+    {
+        public void Run()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
